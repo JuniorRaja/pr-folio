9d98be32669529724055071510542662cbae6038
Prasanna Rajendran <imprasannarajendran@gmail.com>
2025-12-28 18:26:32 +0530

    Fix: Theme based browser tab favicon

--- src/hooks/use-favicon.ts
+++ src/hooks/use-favicon.ts
@@ -2,14 +2,19 @@ import { useEffect } from 'react';
 
 export const useFavicon = (isDark: boolean) => {
   useEffect(() => {
-    const favicon = document.querySelector("link[rel*='icon']") as HTMLLinkElement;
-    const favicon32 = document.querySelector("link[rel='icon'][sizes='32x32']") as HTMLLinkElement;
-    const favicon16 = document.querySelector("link[rel='icon'][sizes='16x16']") as HTMLLinkElement;
-    
-    const basePath = isDark ? '/favicon-dark' : '/favicon-light';
-    
-    if (favicon) favicon.href = `${basePath}/favicon.ico`;
-    if (favicon32) favicon32.href = `${basePath}/favicon-32x32.png`;
-    if (favicon16) favicon16.href = `${basePath}/favicon-16x16.png`;
+    updateFavicon(isDark);
   }, [isDark]);
+};
+
+// Standalone function that can be called outside React
+export const updateFavicon = (isDark: boolean) => {
+  const favicon = document.querySelector("link[rel*='icon']") as HTMLLinkElement;
+  const favicon32 = document.querySelector("link[rel='icon'][sizes='32x32']") as HTMLLinkElement;
+  const favicon16 = document.querySelector("link[rel='icon'][sizes='16x16']") as HTMLLinkElement;
+  
+  const basePath = isDark ? '/favicon-dark' : '/favicon-light';
+  
+  if (favicon) favicon.href = `${basePath}/favicon.ico`;
+  if (favicon32) favicon32.href = `${basePath}/favicon-32x32.png`;
+  if (favicon16) favicon16.href = `${basePath}/favicon-16x16.png`;
 };
